32ca3b552461b1fc42bc2e489abd49efd6fc3021
STetsing <41009393+STetsing@users.noreply.github.com>
2024-10-10 18:45:11 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -30,41 +30,36 @@ app.use(morgan('common', {
 }));
 app.use(morgan('dev'));
 
-
 //app.use(vhost('*', remixProject()));
-// app.use(vhost('remixproject.org', remixProject()))
-// app.use(vhost('www.remixproject.org', remixProject()))
-// app.use(vhost('embedly.remixproject.org', embedly()))
-// app.use(vhost('*.dyn.plugin.remixproject.org', ipfsPlugin()))
-// app.use(vhost('jqgt.remixproject.org', ipfsGatewayPlugin()))
-// app.use(vhost('corsproxy.remixproject.org', corsProxy()))
-// app.use(vhost('vyper.remixproject.org', vyperProxy()))
-// app.use(vhost('vyper2.remixproject.org', vyper2Proxy()))
-// app.use(vhost('rss.remixproject.org', RSS()))
-// app.use(vhost('status.remixproject.org', StatusPlugin()))
-// app.use(vhost('openai-gpt.remixproject.org', openaigpt()))
-app.use(vhost('solcodertest.org', solcoder()))
-// app.use(vhost('gpt-chat.remixproject.org', gptchat()))
-app.use(vhost('solcodercomptest.org', solcompletion()))
-// app.use(vhost('solidityscan.remixproject.org', solidityScan()))
-// app.use(vhost('github.remixproject.org', gitHub()))
-// app.use(vhost('ghfolderpull.remixproject.org', ghfolderpull()))
-// app.use(vhost('common-corsproxy.remixproject.org', commonCorsProxy()))
+app.use(vhost('remixproject.org', remixProject()))
+app.use(vhost('www.remixproject.org', remixProject()))
+app.use(vhost('embedly.remixproject.org', embedly()))
+app.use(vhost('*.dyn.plugin.remixproject.org', ipfsPlugin()))
+app.use(vhost('jqgt.remixproject.org', ipfsGatewayPlugin()))
+app.use(vhost('corsproxy.remixproject.org', corsProxy()))
+app.use(vhost('vyper.remixproject.org', vyperProxy()))
+app.use(vhost('vyper2.remixproject.org', vyper2Proxy()))
+app.use(vhost('rss.remixproject.org', RSS()))
+app.use(vhost('status.remixproject.org', StatusPlugin()))
+app.use(vhost('openai-gpt.remixproject.org', openaigpt()))
+app.use(vhost('solcoder.remixproject.org', solcoder()))
+app.use(vhost('gpt-chat.remixproject.org', gptchat()))
+app.use(vhost('completion.remixproject.org', solcompletion()))
+app.use(vhost('solidityscan.remixproject.org', solidityScan()))
+app.use(vhost('github.remixproject.org', gitHub()))
+app.use(vhost('ghfolderpull.remixproject.org', ghfolderpull()))
+app.use(vhost('common-corsproxy.remixproject.org', commonCorsProxy()))
 
 // Start the server
 const port = Number(80);
-app.listen(port, () => {
-    logger.info('Express server started on port: ' + port);
-});
+ @@ -59,8 +59,6 @@ app.listen(port, () => {
 
 try {
     const httpsServer = https.createServer({
+        key: fs.readFileSync('/etc/letsencrypt/live/acme.remixproject.org/privkey.pem'),
+        cert: fs.readFileSync('/etc/letsencrypt/live/acme.remixproject.org/fullchain.pem'),
     }, app);
     setupWsServer(httpsServer, '/solidityscan');
-    httpsServer.listen(443, () => {
-        logger.info('HTTPS Server running on port 443');
-    });
-
 } catch (e) {
     console.warn(e)
 }
